71db40e796b9b4eb15ac773cb50dfcc22018b19e
Harikethan9 <harikethanreddy9@gmail.com>
2024-06-17 18:46:43 +0530

    created Divide File

---
+++ divide.java
@@ -0,0 +1,10 @@
+public class divide {
+    public static void main(String[] args) {
+        int a=10;
+        int b=2;
+        int c;
+        c =a/b;
+        System.out.println(c);
+
+    }
+}
