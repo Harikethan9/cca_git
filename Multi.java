0104854340040e67853173fe448e6fcba13bdeb8
Harikethan9 <harikethanreddy9@gmail.com>
2024-06-17 18:41:44 +0530

    created Multi File

---
+++ Multi.java
@@ -0,0 +1,11 @@
+public class Multi {
+    public static void main(String[] args) {
+        int a,b,mul;
+        a = 9;
+        b=5;
+        mul = a*b;
+        System.out.println(mul);
+
+
+    }
+}
